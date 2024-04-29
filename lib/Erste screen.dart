ea54e4703e8d7b60c0dda8ce59ea69aee691c3e4
MelanieAppAkademie <melanie.geyer@app-akademie.com>
2024-04-29 14:30:59 +0200

    hab deinen Code gefixed :)

--- lib/Erste screen.dart
+++ lib/Erste screen.dart
@@ -4,7 +4,6 @@ void main() {
   runApp(const MainApp());
 }
 
-
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
@@ -12,40 +11,27 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      
       home: Scaffold(
-        body: Stack(
-          children: [
-            // Background image that fills the entire screen
-            SizedBox.expand(
-              child: Image.asset('assets/hintergruende/bg_startscreen.png'),
-            ),
-            // Draggable image
-            Positioned(
-              top: 90.0,
-              left: 20.0,
-              child: Image.asset('assets/bg_startscreen.png'),
-                   // Updated the path
-                
-                ),
-          
-          
-               ]
-               ),
-        
-              ),
-        
-            // Container with transparent background and content
-            Positioned(
+        body: Stack(children: [
+          // Background image that fills the entire screen
+          SizedBox.expand(
+            child: Image.asset('assets/hintergruende/bg_startscreen.png'),
+          ),
+          // Draggable image
+          Positioned(
+            top: 90.0,
+            left: 20.0,
+            child: Image.asset('assets/bg_startscreen.png'),
+            // Updated the path
+          ),
+          Positioned(
               left: 16.0,
               right: 16.0,
               bottom: 90.0,
               child: Container(
-                height: 200.0,
-                width: double.infinity,
                 decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(30.0),
-                  gradient: LinearGradient(
+                  borderRadius: BorderRadius.circular(30),
+                  gradient: const LinearGradient(
                     begin: Alignment.topLeft,
                     end: Alignment.bottomRight,
                     colors: [
@@ -55,11 +41,11 @@ class MainApp extends StatelessWidget {
                   ),
                 ),
                 child: Padding(
-                  padding: const EdgeInsets.all(16.0),
+                  padding: EdgeInsets.all(16.0),
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Text(
+                      const Text(
                         'Feeling Snackish Today?',
                         style: TextStyle(
                           color: Colors.white,
@@ -67,42 +53,51 @@ class MainApp extends StatelessWidget {
                           fontWeight: FontWeight.bold,
                         ),
                       ),
-                      Text(
+                      const Text(
                         'Explore Angi\'s most popular snack selection and get instantly happy.',
                         style: TextStyle(
                           color: Colors.white,
                           fontSize: 16.0,
                         ),
                       ),
-                      
-                      ElevatedButton(onPressed: () {
-                          // Handle button press
-                        },child:Text('Order Now', style: TextStyle(color: Color.fromARGB(255, 231, 36, 153),), ElevatedButton.styleFrom()
-                        style: 
-                          // ignore: prefer_const_constructors
-                  
-                          backgroundColor: Color.fromARGB(255, 255, 255, 255),
-                      
-                      
-                  
-                           shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10.0),
-                        
-                        child: Text('Order Now', style: TextStyle(color: Color.fromARGB(255, 231, 36, 153),),
-                      
-                      
-                          side: BorderSide(
-                            color: Colors.white,
-                            width: 2.0,
-                          ),
-                        ),
-                      ),
+                      ElevatedButton(
+                          onPressed: () {
+                            // Hier Button Funktionalität implementieren
+                          },
+                          child: const Text('Order Now',
+                              style: TextStyle(
+                                color: Color.fromARGB(255, 231, 36, 153),
+                                backgroundColor: Color.fromARGB(255, 255, 255, 255)
+                              )))
                     ],
                   ),
                 ),
-              ),
-            ),
-          ],
-        ),
+              ))
+        ]),
+      ),
+    );
   }
 }
+        
+            // Container with transparent background and content
+           // Positioned(
+              // 
+              // child: Container(
+              //   
+          
+              //   child: const Padding(
+              //     padding: const EdgeInsets.all(16.0),
+              //     child: Column(
+              //       mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              //       children: [
+              //         
+              //         
+                      
+                      // ElevatedButton(onPressed: () {
+                      //     // Handle button press
+                      //   },child:Text('Order Now', style: TextStyle(color: Color.fromARGB(255, 231, 36, 153),), ElevatedButton.styleFrom()
+                      //   style: 
+                      //     // ignore: prefer_const_constructors
+                  
+                      //     ,
+
